c1ce55c06f84dadc9e715360076ac8e2a6e8431d
kenji <kenjix12x22@gmail.com>
2021-01-25 23:14:41 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+//action = {
+//  type: 'CREATE_EVENT'
+//  title: '2020年東京オリンピックのお知らせ'
+//  body: '2020年に東京でオリンピックを開催します！つきましては、、、'
+//}
+
+const events = (state = [], action) => {
+  switch(action.type){
+    case 'CREATE_EVENT': 
+      const event = {title: action.title, body: action.body }  
+      const length = state.length
+      const id = length === 0 {id = 1} else {id = state[length - 1].id + 1}
+      
+    case 'DELETE_EVENT':
+      return state
+    case 'DELETE_ALL_EVENTS': 
+      return []
+    default:
+      return state
+  }
+}
+
+export default events
