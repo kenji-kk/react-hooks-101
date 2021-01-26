da1c89807450d45aabaf4485599e4983fa4016a4
kenji <kenjix12x22@gmail.com>
2021-01-27 00:52:20 +0900

    display all operation logs

---
+++ src/components/OperationLog.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+
+const OperationLog = ({operationLog}) => {
+  return (
+      <tr>
+          <td>{operationLog.description}</td>
+          <td>{operationLog.operatedAT}</td>
+      </tr>
+  )
+}
+
+export default OperationLog
